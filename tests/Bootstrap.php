9cd83d4143ee01ec4dc4f85eb9fd825cd89b1a4b
skylei <skylei@live.com>
2017-08-15 11:51:21 +0800

    add travis files

---
+++ tests/Bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Date: 2017/8/15
+ * Time: 11:38
+ * User: zhanglei <zhanglei6@xin.com>
+ */
+
+$loader = include __DIR__ . '/../vendor/autoload.php';
+$loader->add( 'EvaThumber', __DIR__ . '/../src' );
